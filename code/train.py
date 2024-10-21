c8ea39d284c3820247ab1578518cf7a2fd881486
Corné Verburg <corneverburg@login01.delftblue.tudelft.nl>
2024-10-21 17:55:00 +0200

    Initial commit

---
+++ code/train.py
@@ -0,0 +1,83 @@
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader, Dataset
+from torch.cuda.amp import GradScaler
+
+from models import *
+from utils import parse_args, save_args_to_json, plot_results
+from dataprocessing import DatasetMultipleSubdomains
+from utils.train_utils import *
+from utils.visualization import *
+
+# Load and save the arguments from the arge parser
+args = parse_args()
+try:
+    os.makedirs(args.save_path)
+except:
+    print("Results directory already exists!")
+    exit()
+
+save_args_to_json(args=args, filename=os.path.join(args.save_path, "args.json"))
+
+# Check if we have half precision
+half_precision = torch.cuda.is_available()
+data_type = torch.float16 if half_precision else torch.float32
+
+# Half precision scaler
+scaler = GradScaler(enabled=half_precision)
+
+# Set datasets
+image_dir = "/scratch/e451412/data/test_data/Inputs"
+mask_dir = "/scratch/e451412/data/test_data/Labels"
+
+train_dataset = DatasetMultipleSubdomains(image_labels=[f"RUN_{i}.pt" for i in range(3)], image_dir=image_dir, mask_dir=mask_dir, transform=None,
+                                    target_transform=None, data_augmentation=None, subdomains_dist=args.subdomains_dist, patch_size=640)
+
+val_dataset = DatasetMultipleSubdomains(image_labels=[f"RUN_{i}.pt" for i in range(3)], image_dir=image_dir, mask_dir=mask_dir, transform=None,
+                                    target_transform=None, data_augmentation=None, subdomains_dist=args.subdomains_dist, patch_size=640)
+
+test_dataset = DatasetMultipleSubdomains(image_labels=[f"RUN_{i}.pt" for i in range(3)], image_dir=image_dir, mask_dir=mask_dir, transform=None,
+                                    target_transform=None, data_augmentation=None, subdomains_dist=args.subdomains_dist, patch_size=640)
+
+# Define dataloaders
+dataloader_train = DataLoader(train_dataset, batch_size=args.batch_size_training, shuffle=True) 
+dataloader_val = DataLoader(val_dataset, batch_size=args.batch_size_testing, shuffle=False)
+dataloader_test = DataLoader(test_dataset, batch_size=args.batch_size_testing, shuffle=False)
+
+# Set devices
+devices = [f"cuda:{i}" for i in range(torch.cuda.device_count())] or ["cpu"]
+print("Available GPUs:", devices)
+
+unet, val_losses, training_losses = train_parallel_model(model=MultiGPU_UNet_with_comm, dataloader_val=dataloader_val, dataloader_train=dataloader_train,
+                                                         scaler=scaler, data_type=data_type, half_precision=True, train_dataset=train_dataset, val_dataset=val_dataset,
+                                                         comm=args.comm, num_epochs=args.num_epochs, num_comm_fmaps=args.num_comm_fmaps, 
+                                                         save_path=args.save_path, subdomains_dist=args.subdomains_dist, exchange_fmaps=args.exchange_fmaps,
+                                                         padding=args.padding, depth=args.depth, kernel_size=args.kernel_size, communication_network=None,
+                                                         complexity=args.complexity, dropout_rate=0.0, devices=devices, num_convs=args.num_convs)
+
+plot_results(unet=unet, savepath=args.save_path, epoch_number="best", train_dataset=train_dataset, val_dataset=val_dataset)
+
+# Save the losses
+data = {
+    "val_losses": val_losses,
+    "training_losses": training_losses
+}
+
+# Save to a JSON file
+with open(os.path.join(args.save_path, 'losses.json'), 'w') as json_file:
+    json.dump(data, json_file)
+
+# Call the function to compute average training loss and plot losses
+avg_training_losses = average_training_losses(training_losses=training_losses, 
+                                              dataloader_train=dataloader_train, 
+                                              num_epochs=args.num_epochs)
+
+plot_losses(avg_training_losses=avg_training_losses, val_losses=val_losses, save_path=args.save_path)
+print("Finished!")
+
+
---
+++ code/utils/visualization.py
@@ -0,0 +1,71 @@
+import os
+import torch
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_results(unet, savepath, epoch_number, train_dataset, val_dataset):
+    def plot_subplot(position, image, title='', vmin=None, vmax=None):
+        plt.subplot(4, 3, position)
+        plt.axis("off")
+        plt.imshow(image, cmap="RdBu_r", vmin=vmin, vmax=vmax)
+        # plt.colorbar(shrink=0.5)
+        # if title:
+        #     plt.title(title)
+
+    def process_and_plot(images, masks, start_pos,):
+        unet.eval()
+        with torch.no_grad():
+            predictions = unet([img.unsqueeze(0) for img in images]).cpu()
+            full_images = unet.concatenate_tensors([img.unsqueeze(0) for img in images]).squeeze().cpu()
+
+        for i in range(3):
+            plot_subplot(start_pos + i, full_images[i].cpu())
+        
+        plot_subplot(start_pos + 3, predictions[0, 0].cpu(), vmin=0, vmax=1)
+        plot_subplot(start_pos + 4, masks.cpu()[0])
+        plot_subplot(start_pos + 5, torch.abs(masks.cpu()[0] - predictions[0, 0].cpu()), vmin=0, vmax=1)
+
+    plt.figure(figsize=(9, 12))
+    
+    # Adjust spacing between plots
+    plt.subplots_adjust(hspace=0.1, wspace=0.1)
+
+    train_image, train_mask = train_dataset[0]
+    process_and_plot(train_image, train_mask, 1)
+
+    val_image, val_mask = val_dataset[0]
+    process_and_plot(val_image, val_mask, 7)
+
+    os.makedirs(os.path.join(savepath, "figures"), exist_ok=True)
+    plt.savefig(os.path.join(savepath, "figures", f"epoch_{epoch_number}.png"), bbox_inches='tight')
+    plt.close()
+
+
+# Compute average training loss per epoch
+def average_training_losses(training_losses, dataloader_train, num_epochs):
+    num_batches_per_epoch = len(dataloader_train)
+    avg_training_losses = []
+    
+    for epoch in range(num_epochs):
+        start_idx = epoch * num_batches_per_epoch
+        end_idx = start_idx + num_batches_per_epoch
+        avg_training_losses.append(np.mean(training_losses[start_idx:end_idx]))
+    
+    return avg_training_losses
+
+# Code to plot val and training loss in log-log scale
+def plot_losses(avg_training_losses, val_losses, save_path):
+    plt.figure(figsize=(10, 6))
+    
+    # Plot training and validation losses on a log-log scale
+    plt.semilogy(avg_training_losses, label='Training Loss (Per Epoch)', color='blue')
+    plt.semilogy(val_losses, label='Validation Loss', color='red')
+    
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.title('Training and Validation Loss (Log-Log Scale)')
+    plt.legend()
+    
+    # Save the plot to the save_path directory
+    plt.savefig(os.path.join(save_path, 'losses_loglog.png'))
+    plt.close()
